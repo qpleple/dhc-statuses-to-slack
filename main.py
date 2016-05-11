61c1c0c6c4e972f16e4a7986626ae17fbaabe689
Quentin Pleplé <quentin.pleple@gmail.com>
2016-05-11 14:11:56 -0700

    fixed

--- main.py
+++ main.py
@@ -16,6 +16,12 @@ states = {
     '2042': {'label': ":inbox_tray: Plus de papier"},
     '2043': {'label': ":japanese_goblin: Bourrage papier"},
     '2044': {'label': ":printer: Imprimante déconnectée"},
+    '3012': {'label': ":speech_balloon: Initialisation (mode test)"},
+    '3021': {'label': ":white_check_mark: OK (mode test)"},
+    '3032': {'label': ":zzz: Sleep (mode test)"},
+    '3042': {'label': ":inbox_tray: Plus de papier (mode test)"},
+    '3043': {'label': ":japanese_goblin: Bourrage papier (mode test)"},
+    '3044': {'label': ":printer: Imprimante déconnectée (mode test)"},
 }
 
 def get_new_rows(last_id):
@@ -33,20 +39,40 @@ def get_new_rows(last_id):
 def post_message(data):
     requests.post(config.slack['hook_url'], data=json.dumps(data))
 
-def handle_new_row(row):
+def post_error(msg):
     post_message({
-      'username': "%s (%s)" % (row['name'], row['d_name']),
-      'text': "<http://short-edition.com/admin/distributeur/%s/show|%s>" % (row['d_id'], states[row['state']]['label']),
+      'username': "Erreur",
+      'text': msg,
       'icon_emoji': ':dhc:',
-      'channel': '#statuts' if row['mode'] == 'prod' else '#statuts-preprod',
+      'channel': '#statuts-preprod' if 'mode' in row and mode['row'] == 'preprod' else '#statuts',
+    })
+
+def handle_new_row(row):
+    name = row['name'] if 'name' in row else "<name>"
+    d_name = row['d_name'] if 'd_name' in row else "<d_name>"
+    d_id = row['d_name'] if 'd_id' in row else "<d_id>"
+    state = states[row['state']]['label'] if 'state' in row and row['state'] in states else "<state>"
+    channel = '#statuts-preprod' if 'mode' in row and mode['row'] == 'preprod' else '#statuts'
+
+
+    post_message({
+      'username': "%s (%s)" % (name, d_name),
+      'text': "<http://short-edition.com/admin/distributeur/%s/show|%s>" % (d_id, state),
+      'icon_emoji': ':dhc:',
+      'channel': channel,
     })
 
 last_id = db.get("SELECT id FROM dispenser_log_status ORDER BY id DESC LIMIT 1")['id']
 while True:
-    rows = get_new_rows(last_id)
-    if rows:
-        for row in rows:
-            handle_new_row(row)
-        last_id = rows[-1]['id']
+    try:
+        rows = get_new_rows(last_id)
+        if rows:
+            for row in rows:
+                handle_new_row(row)
+                
+            last_id = rows[-1]['id']
+    except Exception, e:
+        post_error(e.message)
+        sleep(60)
     
     sleep(3)
